f7261e31ebb7c9de3cc7fed67a7cff8af983dff5
tarihartono04 <taridwisyafitrih@gmail.com>
2025-06-02 13:26:49 +0700

    menambahkan foto dan mengganti foto portofolio

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -183,9 +183,9 @@ if (isset($media['data']) && count($media['data']) >= 1) {
 
           <div class="col-md mb-4">
             <div class="card">
-              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
+              <img class="card-img-top" src="img/thumbs/10.png" alt="Card image cap">
               <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Magang di PTPN IV Regional I.</p>
               </div>
             </div>
           </div>
